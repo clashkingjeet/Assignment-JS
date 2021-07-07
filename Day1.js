3262e131e5332276bfc21a14bd9849afcbe8780a
clashkingjeet <87075351+clashkingjeet@users.noreply.github.com>
2021-07-07 18:19:32 +0530

    Update Day1.js

--- Day1.js
+++ Day1.js
@@ -1,1 +1,12 @@
-console.log("* * * * *");
+
+//Print 5 star pattern
+// Day 1 date:july 6 
+str="";
+for(i=0;i<5;i=i+1)
+{
+  str+="* ";
+}
+console.log(str);
+
+//console.log("* * * * *");
+//output:* * * * *
